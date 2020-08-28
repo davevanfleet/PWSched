badd74ccee2e754e9c9cbda83fd8abc5e87bb9de
dvfleet413 <dvfleet413@gmail.com>
2020-08-27 21:05:06 -0400

    working on redux

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,9 @@
 export function userReducer(state={}, action){
+
     switch(action.type){
         case 'ASSIGN_CURRENT_USER':
             return {
                 ...action.user,
-                recipes: action.user.recipes.map(recipe => recipe.id)
             }
         case 'LOGOUT':
             return {}
